84de05ceb346d0ccc8a735449a441f3d59cb7c26
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-27 14:51:53 +0530

    feature #132 driving car with keys

---
+++ main.js
@@ -0,0 +1,86 @@
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=window.innerWidth - 330;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
+
+
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+
+
+const N=1;
+const cars=generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
+            localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.1);
+        }
+    }
+}
+
+const traffic=[];
+const roadBorders = [];
+
+animate();
+
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N){
+    
+    
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(100,100,30,50,"KEYS"));
+    }
+    return cars;
+}
+
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(roadBorders,[]);
+    }
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(roadBorders,traffic);
+    }
+    bestCar=cars.find(
+        c=>c.fittness==Math.max(
+            ...cars.map(c=>c.fittness)
+        ));
+
+carCanvas.height=window.innerHeight;
+networkCanvas.height=window.innerHeight;
+
+   carCtx.save();
+   carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
+
+
+   for(let i=0;i<traffic.length;i++){
+    traffic[i].draw(carCtx);
+   }
+   carCtx.globalAlpha=0.2;
+   for(let i=0;i<cars.length;i++){
+    cars[i].draw(carCtx);
+   }
+   carCtx.globalAlpha=1;
+   bestCar.draw(carCtx,true);
+
+   carCtx.restore();
+
+   networkCtx.lineDashOffset=-time/50;
+   networkCtx.clearRect(0, 0, networkCanvas.width, networkCanvas.height);
+   Visualizer.drawNetwork(networkCtx,bestCar.brain);
+   requestAnimationFrame(animate);
+
+}
