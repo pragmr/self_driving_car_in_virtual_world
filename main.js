fc3b1464a48326167dad523cc14db1592c4ac288
sanch27 <sanchaliannavarapu@gmail.com>
2024-04-30 07:47:11 +0530

    feature #135 added osm button with osm data

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ carCanvas.width=window.innerWidth - 330;
 const networkCanvas=document.getElementById("networkCanvas");
 networkCanvas.width=300;
 
+carCanvas.height=window.innerHeight;
+networkCanvas.height=window.innerHeight;
+
 
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
+
+
+const worldString = localStorage.getItem("world");
+const worldInfo = worldString ? JSON.parse(worldString) : null;
+const world = worldInfo
+   ? World.load(worldInfo)
+   : new World(new Graph());
+const viewport = new Viewport(carCanvas, world.zoom, world.offset);
 
 
 
@@ -24,7 +35,7 @@ if(localStorage.getItem("bestBrain")){
 }
 
 const traffic=[];
-const roadBorders = [];
+const roadBorders = world.roadBorders.map((s) => [s.p1, s.p2]);
 
 animate();
 
@@ -38,11 +49,20 @@ function discard(){
 }
 
 function generateCars(N){
-    
+
+    const startPoints = world.markings.filter((m) => m instanceof Start);
+    const startPoint = startPoints.length > 0
+        ? startPoints[0].center
+        : new Point(100, 100);
+    const dir = startPoints.length > 0
+        ? startPoints[0].directionVector
+        : new Point(0, -1);
+    const startAngle = - angle(dir) + Math.PI / 2; 
+  
     
     const cars=[];
     for(let i=1;i<=N;i++){
-        cars.push(new Car(100,100,30,50,"KEYS"));
+        cars.push(new Car(startPoint.x, startPoint.y,30,50,"KEYS",startAngle ));
     }
     return cars;
 }
@@ -59,24 +79,26 @@ function animate(time){
             ...cars.map(c=>c.fittness)
         ));
 
-carCanvas.height=window.innerHeight;
-networkCanvas.height=window.innerHeight;
+        world.cars = cars;
+        world.bestCar = bestCar;
 
-   carCtx.save();
-   carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
+        viewport.offset.x = -bestCar.x;
+        viewport.offset.y = -bestCar.y;
+
+
+
+viewport.reset();
+ const viewPoint = scale(viewport.getOffset(), -1);
+ world.draw(carCtx, viewPoint, false);
+
+  
 
 
    for(let i=0;i<traffic.length;i++){
     traffic[i].draw(carCtx);
    }
-   carCtx.globalAlpha=0.2;
-   for(let i=0;i<cars.length;i++){
-    cars[i].draw(carCtx);
-   }
-   carCtx.globalAlpha=1;
-   bestCar.draw(carCtx,true);
+  
 
-   carCtx.restore();
 
    networkCtx.lineDashOffset=-time/50;
    networkCtx.clearRect(0, 0, networkCanvas.width, networkCanvas.height);
